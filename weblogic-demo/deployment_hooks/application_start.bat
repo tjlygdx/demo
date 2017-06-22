c5ddf3bc01931e09400e5dce54d90ee4b37d55b4
taojinlong <tjlygdx@163.com>
2017-06-22 21:38:09 +0800

    deployment_hooks

--- weblogic-demo/deployment_hooks/application_start.bat
+++ weblogic-demo/deployment_hooks/application_start.bat
@@ -1,6 +1,2 @@
 echo application_start.bat
 
-SET WLS_HOME=C:\Oracle\Middleware\Oracle_Home\wlserver\server
-SET CLS_PATH=%WLS_HOME%\lib\weblogic.jar;.
-CMD /c %WLS_HOME%\bin\setWLSEnv.cmd
-java -classpath %CLS_PATH% weblogic.Deployer -adminurl http://127.0.0.1:7001 -user weblogic -password fit2cloud -start -name xwtest
