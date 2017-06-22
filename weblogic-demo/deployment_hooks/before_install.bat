7fe87bfcd8767011513e7ad9882f9a4641227f2c
taojinlong <tjlygdx@163.com>
2017-06-22 21:33:32 +0800

    test

--- weblogic-demo/deployment_hooks/before_install.bat
+++ weblogic-demo/deployment_hooks/before_install.bat
@@ -1,5 +1,5 @@
 echo before_install.bat
 
-set DIRECTORY=c:\tmp\weblogic-demo
-if exist %DIRECTORY% rd /s /q %DIRECTORY%
-md %DIRECTORY%
+#set DIRECTORY=c:\tmp\weblogic-demo
+##if exist %DIRECTORY% rd /s /q %DIRECTORY%
+#md %DIRECTORY%
